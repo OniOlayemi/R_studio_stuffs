4a02bb0336570ab28c5ca4947d464716534cbc38
OniOlayemi <163363507+OniOlayemi@users.noreply.github.com>
2024-04-13 22:24:40 +0100

    Add files via upload

---
+++ day1 challenge.R
@@ -0,0 +1,97 @@
+?haven
+?xlsx
+library(haven)
+library(xlsx)
+
+library(help= 'datasets' )
+View(volcano)
+View(AirPassengers)
+
+
+?datase
+?datasets
+
+
+cat('I cannot wait till lunchtime\n', "die another day\n")
+
+cat("#This is a comment\n #This is another comment\n")
+#this is a comment
+## #comment
+## #I cannot wait till lunchtime
+#new new
+
+## #comment
+
+Rscript first.R
+cat(Rscript, file = first.R)
+cat(file = first.R)
+first.R
+open.srcfile(first.R)
+
+
+getwd()
+
+write.csv(untitled1, vssr.csv)
+
+saveRDS()
+readRDS()
+
+install.packages('Rtools')
+library(haven)
+library(xlsx)
+
+read.xl
+
+read_
+
+library(help = 'datasets')
+
+view(iris)
+View
+head(iris)
+diris <- iris[,,]
+diris[, "Sepal.Length"]
+
+install.packages(c('Hmisc','pastecs', 'psych', 'skimr', 'summarytools' ))
+
+rea
+
+merge(akdfk,ldlf,)
+?merge
+
+dunno <- function(a, b) { ifelse(a < b, -1, ifelse(a>b, 1, 0))}
+
+dunno(2:500:2, 0:25**2)
+
+dunno<- Vectorize(dunno, vectorize.args = 'a')
+
+Make_node <- function(name, left = NULL, right = NULL)
+  list(name = name, left = left, right = right)
+tree <- make_node("root",
+                  make_node("C", make_node("A"),
+                            make_node("B")),
+                  make_node("D"))
+
+'if'(1 < 3, 'true', 'false')
+'+'(1,4)  
+
+'%x%' <- '*'
+
+4 %x% 2
+
+library(pryr)
+
+install.packages(pryr)
+
+memory.size(x <- 1:6)
+
+
+x <- 1:4
+
+attributes(x) <- list(foo = 'bar')
+
+attributes(x)
+
+attr(x, 'baz') <- 'qux'
+
+attributes(x)
